5472659a4a9fe9c4c361b7dd25337e30a757ded9
William Carrel <wac@gmail.com>
2021-12-11 19:38:21 -0800

    2021-12-11 part 1 complete.
    --HG--
    branch : main

--- 2021/2021-12-11.py
+++ 2021/2021-12-11.py
@@ -1,2 +1,61 @@
+MAX_STEPS = 100
+SANITY_CHECKS = True
+NEIGHBORS = ((-1,1), (0,1), (1,1), (-1,0), (1,0), (-1,-1), (0,-1), (1,-1))
+
+octo_array: list[list[int]] = []
+
+step = -1
+total_flash_count = 0
+
 with open('2021-12-11.txt') as f:
-    
+    for line in f:
+        octo_array.append([int(c) for c in line.strip()])
+
+if SANITY_CHECKS:
+    # The input is a square with no shenanigans.
+    assert 1 == len(set(len(x) for x in octo_array))
+    assert len(octo_array) == len(octo_array[0])
+
+for step in range(MAX_STEPS):
+    pass
+    # Increment energy levels everywhere in the array
+    flash_stack: list[tuple[int,int]] = []
+
+    for row in range(len(octo_array)):
+        for col in range(len(octo_array[row])):
+            octo_array[row][col] += 1
+            if octo_array[row][col] > 9:
+                flash_stack.append((row,col))
+
+    # Do flashes
+    flashed: list[list[bool]] = [
+        [False for _ in row] for row in octo_array
+    ]
+
+    if SANITY_CHECKS: assert len(flashed) == len(flashed[0]) == len(octo_array)
+    while flash_stack:
+        row, col = flash_stack.pop()
+        # Already flashed? Skip to next.
+        if flashed[row][col]:
+            continue
+        flashed[row][col] = True
+        # Increment neighbors.
+        for nrow, ncol in NEIGHBORS:
+            # Keep us inbounds.
+            if min(row+nrow, col+ncol) < 0: continue
+            if max(row+nrow, col+ncol) >= len(octo_array): continue
+            octo_array[row+nrow][col+ncol] += 1
+            if octo_array[row+nrow][col+ncol] > 9:
+                flash_stack.append((row+nrow,col+ncol))
+
+    # Set flashed to energy to zero
+    flash_count = 0
+    for row in range(len(octo_array)):
+        for col in range(len(octo_array[row])):
+            if flashed[row][col]:
+                octo_array[row][col] = 0
+                flash_count += 1
+
+    total_flash_count += flash_count
+
+print('step {} total flash count: {}'.format(step+1, total_flash_count))
